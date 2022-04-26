ecc0abda41713841d5315d80d13a874006524ba1
rmjohns77077 <Ryanmj7707@outlook.com>
2022-04-25 23:23:28 -0400

    Add project files.

---
+++ movies1/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace movies1
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Genre { get; set; }
+    }
+}
---
+++ movies1/Program.cs
@@ -0,0 +1,25 @@
+﻿using movies1;
+
+var DumbandDumber = new Movie();
+DumbandDumber.Title = "Dumb and Dumber";
+DumbandDumber.Genre = "Comedy";
+
+var Taken=new Movie();
+Taken.Title = "Taken";
+Taken.Genre = "Suspense";
+
+var MightyDucks = new Movie();
+MightyDucks.Title = "Might Ducks";
+MightyDucks.Genre = "Comedy";
+
+List<Movie>movies=new List<Movie>();
+movies.Add(DumbandDumber);
+movies.Add(Taken);  
+movies.Add(MightyDucks);
+
+Console.WriteLine("What Genre are you looking at?");
+var genre =Console.ReadLine();
+var moviesgenre = movies.Where(m => m.Genre == genre);
+
+Console.WriteLine($"Just the  {genre} movies");
+
